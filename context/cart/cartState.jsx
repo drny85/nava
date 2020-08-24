aa0a2513d08e024be15f65595fbe4ddadf4c506d
Robert Melendez <robertm3lendez@gmail.com>
2020-08-24 15:39:15 -0400

    fix issue with adding and deleting from in cart

--- context/cart/cartState.jsx
+++ context/cart/cartState.jsx
@@ -10,237 +10,280 @@ import ShoppingCart from "../../models/ShoppingCart";
 import CarItem from "../../models/CartItem";
 
 const CartState = (props) => {
-  const initialState = {
-    cartItems: [],
-    cartTotal: 0,
-    loading: false,
-    itemCounts: 0,
-  };
+	const initialState = {
+		cartItems: [],
+		cartTotal: 0,
+		loading: false,
+		itemCounts: 0,
+	};
 
-  const CART_ID = "cartId";
+	const CART_ID = "cartId";
 
-  const [state, dispatch] = useReducer(CartReducer, initialState);
+	const [state, dispatch] = useReducer(CartReducer, initialState);
 
-  const addToCart = async (item) => {
-    try {
-      setLoading();
-      const cartId = await getCartId();
+	const addToCart = async (item) => {
+		try {
+			setLoading();
+			const cartId = await getCartId();
 
-      const { items, quantity, total } = (
-        await db.collection("carts").doc(cartId).get()
-      ).data();
+			const { items, quantity, total } = (
+				await db.collection("carts").doc(cartId).get()
+			).data();
 
-      const found = items.find((i) => i.id === item.id);
+			if (item.size) {
+				const itemSizeFound = items.find(
+					(i) => i.id === item.id && i.size === item.size
+				);
 
-      if (found) {
-        console.log("ITEM FOUND");
-        if (item.size) {
-          console.log("Item found and has size");
+				if (itemSizeFound) {
+					console.log("item found");
+					//item found with same size -- update quntity
+					const index = items.findIndex(
+						(i) => i.id === item.id && i.size === item.size
+					);
 
-          const index = items.findIndex(
-            (i) => item.id === i.id && i.size === item.size
-          );
-          const newItem = items[index];
-          if (newItem) {
-            //item in cart and comes in sizes;
-            if (item.size === newItem.size && item.id === newItem.id) {
-              newItem.quantity += 1;
+					const newItems = [...items];
+					newItems[index].quantity = itemSizeFound.quantity + 1;
 
-              await db
-                .collection("carts")
-                .doc(cartId)
-                .update({
-                  items: items,
-                  quantity: quantity + 1,
-                  total: +(total + +item.price).toFixed(2),
-                });
-            }
-          } else {
-            //item in cart and comes in sizes but is the same size
+					await db
+						.collection("carts")
+						.doc(cartId)
+						.update({
+							items: newItems,
+							quantity: quantity + 1,
+							total: +(total + +item.price).toFixed(2),
+						});
+				} else {
+					console.log("item not found");
+					await db
+						.collection("carts")
+						.doc(cartId)
+						.update({
+							items: [...items, item],
+							quantity: quantity + 1,
+							total: +(total + +item.price).toFixed(2),
+						});
 
-            console.log("same size but new");
+					//item not found with same size -- add to the array
+				}
+			} else {
+				const itemFound = items.find((i) => i.id === item.id);
+				if (itemFound) {
+					//item found -- update
+					const index = items.findIndex((i) => i.id === item.id);
+					const newItems = [...items];
+					newItems[index].quantity = itemFound.quantity + 1;
 
-            await db
-              .collection("carts")
-              .doc(cartId)
-              .update({
-                items: [...items, item],
-                quantity: quantity + 1,
-                total: +(total + +item.price).toFixed(2),
-              });
-          }
-        } else {
-          //item in cart but does not come in sizes;
+					await db
+						.collection("carts")
+						.doc(cartId)
+						.update({
+							items: newItems,
+							quantity: quantity + 1,
+							total: +(total + +item.price).toFixed(2),
+						});
+				} else {
+					//item no found -- add
+					await db
+						.collection("carts")
+						.doc(cartId)
+						.update({
+							items: [...items, item],
+							quantity: quantity + 1,
+							total: +(total + +item.price).toFixed(2),
+						});
+				}
+			}
 
-          const updatedItem = { ...item, quantity: found.quantity + 1 };
+			getCartItems();
+		} catch (error) {
+			console.log("Error Adding to Cart", error);
+		}
+	};
 
-          const newItems = items.map((i) =>
-            i.id === item.id ? updatedItem : item
-          );
+	const deleteFromCart = async (item) => {
+		try {
+			const data = await AsyncStorage.getItem(CART_ID);
+			const id = JSON.parse(data);
+			const { items, quantity, total } = (
+				await db.collection("carts").doc(id).get()
+			).data();
 
-          await db
-            .collection("carts")
-            .doc(cartId)
-            .update({
-              items: newItems,
-              quantity: quantity + 1,
-              total: +(total + +item.price).toFixed(2),
-            });
-        }
-      } else {
-        // item not in cart
-        console.log("NO ITEM");
-        if (item.size) {
-          //item not in cart and come in sizes
+			if (item.size) {
+				//item has size -- delete one and update
+				const itemFoundWithSize = items.find(
+					(i) => i.id === item.id && i.size === item.size
+				);
 
-          const newItems = [...items, item];
-          await db
-            .collection("carts")
-            .doc(cartId)
-            .update({
-              items: newItems,
-              quantity: quantity + 1,
-              total: +(total + +item.price).toFixed(2),
-            });
-        } else {
-          //item not in cart and does not come in sizes
-          const newItems = [...items, item];
-          await db
-            .collection("carts")
-            .doc(cartId)
-            .set({
-              items: newItems,
-              quantity: quantity + 1,
-              total: +(total + +item.price).toFixed(2),
-            });
-        }
-      }
+				if (itemFoundWithSize) {
+					const size = itemFoundWithSize.quantity;
 
-      const data = transforItems(items);
-      getCartItems();
-    } catch (error) {
-      console.log(error);
-    }
-  };
+					if (size > 1) {
+						const index = items.findIndex(
+							(i) => i.id === item.id && i.size === item.size
+						);
+						const updatedItems = [...items];
+						updatedItems[index].quantity = itemFoundWithSize.quantity - 1;
 
-  const deleteFromCart = async (item) => {
-    try {
-      const data = await AsyncStorage.getItem(CART_ID);
-      const id = JSON.parse(data);
-      const { items, quantity, total } = (await CART_REF.doc(id).get()).data();
-      const found = items[item.id];
-      if (found.quantity > 1) {
-        --found.quantity;
+						await db
+							.collection("carts")
+							.doc(id)
+							.update({
+								items: updatedItems,
+								quantity: quantity - 1,
+								total: +(total - +item.price).toFixed(2),
+							});
+					} else {
+						// remove from cart
+						const index = items.findIndex(
+							(i) => i.id === item.id && i.size === size
+						);
+						const updatedItems = [...items];
+						updatedItems.splice(index, 1);
 
-        await db
-          .collection("carts")
-          .doc(id)
-          .update({
-            items: { ...items, [item.id]: found },
-            quantity: quantity - 1,
-            total: +(total - item.price).toFixed(2),
-          });
-        getCartItems();
-      } else {
-        delete items[item.id];
-        await db
-          .collection("carts")
-          .doc(id)
-          .update({
-            items: { ...items },
-            quantity: quantity - 1,
-            total: +(total - item.price).toFixed(2),
-          });
+						await db
+							.collection("carts")
+							.doc(id)
+							.update({
+								items: updatedItems,
+								quantity: quantity - 1,
+								total: +(total - +item.price).toFixed(2),
+							});
+					}
+				} else {
+					throw new Error("item not found in cart");
+				}
 
-        getCartItems();
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+				getCartItems();
+			} else {
+				//item has no size -- delete one and update
 
-  const getCartItems = async () => {
-    try {
-      setLoading();
-      const id = await getCartId();
-      const { items, total, quantity } = (
-        await db.collection("carts").doc(id).get()
-      ).data();
+				const itemFound = items.find((i) => i.id === item.id);
+				if (itemFound) {
+					const size = itemFound.quantity;
+					if (size > 1) {
+						//decrease quantity by one
+						const index = items.findIndex((i) => i.id === item.id);
+						const updatedItems = [...items];
+						updatedItems[index].quantity = itemFound.quantity - 1;
 
-      const data = transforItems(items);
+						await db
+							.collection("carts")
+							.doc(id)
+							.update({
+								items: updatedItems,
+								quantity: quantity - 1,
+								total: +(total - +item.price).toFixed(2),
+							});
+					} else {
+						//remove item from array;
 
-      dispatch({
-        type: GET_CART_ITEMS,
-        payload: { data, count: quantity, total },
-      });
-    } catch (error) {
-      console.log("Error from cart", error);
-    }
-  };
+						const index = items.findIndex((i) => i.id === item.id);
+						const updatedItems = [...items];
+						updatedItems.splice(index, 1);
 
-  const clearCart = async () => {
-    try {
-      setLoading();
-      const d = await AsyncStorage.getItem(CART_ID);
-      const id = JSON.parse(d);
-      if (id) {
-        await db.collection("carts").doc(id).delete();
-        await AsyncStorage.removeItem(CART_ID);
-      }
-      dispatch({ type: CLEAR_CART });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+						await db
+							.collection("carts")
+							.doc(id)
+							.update({
+								items: updatedItems,
+								quantity: quantity - 1,
+								total: +(total - +item.price).toFixed(2),
+							});
+					}
 
-  const getCartId = async () => {
-    try {
-      let cartId;
-      const data = await AsyncStorage.getItem(CART_ID);
+					getCartItems();
+				} else {
+					throw new Error("Item not found in cart");
+				}
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-      if (data === null) {
-        const query = await db
-          .collection("carts")
-          .add({ items: [], quantity: 0, total: 0 });
-        cartId = (await query.get()).id;
-        await AsyncStorage.setItem(CART_ID, JSON.stringify(cartId));
-      } else {
-        cartId = JSON.parse(data);
-      }
+	const getCartItems = async () => {
+		try {
+			setLoading();
+			const id = await getCartId();
+			const { items, total, quantity } = (
+				await db.collection("carts").doc(id).get()
+			).data();
 
-      return cartId;
-    } catch (error) {
-      console.log("Error from get cart by Id", error);
-    }
-  };
+			const data = transforItems(items);
 
-  const transforItems = (items) => {
-    let data = [];
-    for (const item in items) {
-      data.push(items[item]);
-    }
+			dispatch({
+				type: GET_CART_ITEMS,
+				payload: { data, count: quantity, total },
+			});
+		} catch (error) {
+			console.log("Error from cart", error);
+		}
+	};
 
-    return data;
-  };
+	const clearCart = async () => {
+		try {
+			setLoading();
+			const d = await AsyncStorage.getItem(CART_ID);
+			const id = JSON.parse(d);
+			if (id) {
+				await db.collection("carts").doc(id).delete();
+				await AsyncStorage.removeItem(CART_ID);
+			}
+			dispatch({ type: CLEAR_CART });
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  const setLoading = () => dispatch({ type: SET_LOADING });
+	const getCartId = async () => {
+		try {
+			let cartId;
+			const data = await AsyncStorage.getItem(CART_ID);
 
-  return (
-    <CartContext.Provider
-      value={{
-        cartItems: state.cartItems,
-        loading: state.loading,
-        cartTotal: state.cartTotal,
-        itemCounts: state.itemCounts,
-        addToCart,
-        clearCart,
-        getCartItems,
-        deleteFromCart,
-      }}
-    >
-      {props.children}
-    </CartContext.Provider>
-  );
+			if (data === null) {
+				const query = await db
+					.collection("carts")
+					.add({ items: [], quantity: 0, total: 0 });
+				cartId = (await query.get()).id;
+				await AsyncStorage.setItem(CART_ID, JSON.stringify(cartId));
+			} else {
+				cartId = JSON.parse(data);
+			}
+
+			return cartId;
+		} catch (error) {
+			console.log("Error from get cart by Id", error);
+		}
+	};
+
+	const transforItems = (items) => {
+		let data = [];
+		for (const item in items) {
+			data.push(items[item]);
+		}
+
+		return data;
+	};
+
+	const setLoading = () => dispatch({ type: SET_LOADING });
+
+	return (
+		<CartContext.Provider
+			value={{
+				cartItems: state.cartItems,
+				loading: state.loading,
+				cartTotal: state.cartTotal,
+				itemCounts: state.itemCounts,
+				addToCart,
+				clearCart,
+				getCartItems,
+				deleteFromCart,
+			}}
+		>
+			{props.children}
+		</CartContext.Provider>
+	);
 };
 
 export default CartState;
