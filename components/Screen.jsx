41bb0e21ae892cee88a4dd8e71162ecd2c21ebcb
drny85 <robertm3lendez@gmail.com>
2021-02-10 15:28:26 -0500

    addded status bar

--- components/Screen.jsx
+++ components/Screen.jsx
@@ -2,10 +2,16 @@
 import React from "react";
 import { View, StyleSheet, SafeAreaView } from "react-native";
 import Constants from "expo-constants";
+import { StatusBar } from 'expo-status-bar';
+
 
 const Screen = ({ children, style }) => {
   return (
-    <SafeAreaView style={[styles.container, style]}>{children}</SafeAreaView>
+    <SafeAreaView style={[styles.container, style]}>
+
+      {children}
+      <StatusBar style='auto' />
+    </SafeAreaView>
   );
 };
 
